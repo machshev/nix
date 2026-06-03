df1b7f6fc1c01bf617a16107b18cd04fb54759d7
David James McCorrie <james.mccorrie@lowrisc.org>
2026-06-03 09:07:54 +0100

    feat: add gemini/grok cli
    Signed-off-by: David James McCorrie <james.mccorrie@lowrisc.org>

--- modules/home-manager/ai.nix
+++ modules/home-manager/ai.nix
@@ -1,11 +1,9 @@
-{
-  pkgs,
-  pkgs-unstable,
-  ...
-}: {
+{pkgs-unstable, ...}: {
   home.packages = [
     pkgs-unstable.claude-code
-    pkgs.opencode
+    pkgs-unstable.opencode
+    pkgs-unstable.gemini-cli
+    pkgs-unstable.grok-cli
     # pkgs.claude-code-router
   ];
 }
