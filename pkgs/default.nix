a789ac45747f4e77f4403c4b14f5eb5cfb7b1c5b
David James McCorrie <james.mccorrie@lowrisc.org>
2026-07-11 19:02:28 +0100

    feat: workaround for cpplint
    Signed-off-by: David James McCorrie <james.mccorrie@lowrisc.org>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,6 +8,14 @@
 flake-utils.lib.eachDefaultSystemMap (system: let
   pkgs = import nixpkgs-unstable {
     inherit system;
+    overlays = [
+      # cpplint's test suite fails on newer Pythons: codecs.open() now emits
+      # a DeprecationWarning that leaks into stderr, breaking tests that
+      # assert stderr is empty. Not our bug to fix; skip checks.
+      (final: prev: {
+        cpplint = prev.cpplint.overridePythonAttrs (_: {doCheck = false;});
+      })
+    ];
   };
 in {
   dev-udev-rules = pkgs.callPackage ./dev-udev-rules {};
